b800cff0bf574ba7942c41ab68c8ec8498dbf404
Jose Manuel Tuberquia <jmgraciano@globaltask.net>
2024-02-05 12:57:07 -0500

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tuber-code/'
+  base: 'https://tuberquia2115.github.io/tuber-code'
 })
