0a0b5d0fe2a4b5919881e35a2d9c35d5f0392a61
Jose Manuel Tuberquia <jmgraciano@globaltask.net>
2024-02-05 11:26:32 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import logoFronted from './assets/fronted.webp';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <a href="https://github.com/tuberquia2115" target="_blank">
+        <img src={logoFronted} className="logo react" alt="Logo Frontend-developer" />
+      </a>
+      <h1>Portafolio Jose Manuel Graciano Tuberquia</h1>
+    </div>
+  );
+}
+
+export default App;
